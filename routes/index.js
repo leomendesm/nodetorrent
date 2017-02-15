624d0efb5019fcfc98b5cb45a75403d3f9f950ad
Leonardo Mendes MIraglia <leo.mi.me@gmail.com>
2017-02-15 17:49:11 -0200

    editing routes to get new webtorrent

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var Client = require('node-torrent');
-var client = new Client({logLevel: 'DEBUG'});
+var WebTorrent = require('webtorrent')
+var client = new WebTorrent()
+var fs = require('fs');
+var domain = require('domain').create();
 
+function list (){
+	fs.readdir('./public/files',function(error,files){
+	   return files;
+	});
+	domain.on("error",function(erros){
+	   console.log(erros);
+	});
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/torrent', (req, res, next) => {
-var magnet = req.body.torrent;
-var torrent = client.addTorrent(magnet);
-// when the torrent completes, move it's files to another area
-torrent.on('complete', function() {
-    console.log('complete!');
-    torrent.files.forEach(function(file) {
-        var newPath = '/public/files/' + file.path;
-        fs.rename(file.path, newPath);
-        // while still seeding need to make sure file.path points to the right place
-        file.path = newPath;
-		res.render('download', { name: file.path })
-    });
-});
+	var magnetURI = req.body.torrent;
+	client.add(magnetURI, { path: '/public/files' }, function (torrent) {
+	 	torrent.on('done', function () {
+  			res.render('download', { name: list() });
+	  	})
+	})
 })
 module.exports = router;
