9ad91de804de1ab250d099c18e28ec4447846f25
Leonardo Mendes MIraglia <leo.mi.me@gmail.com>
2017-02-15 17:16:30 -0200

    adding some interface

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var Client = require('node-torrent');
+var client = new Client({logLevel: 'DEBUG'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/torrent', (req, res, next) => {
+var magnet = req.body.torrent;
+var torrent = client.addTorrent(magnet);
+// when the torrent completes, move it's files to another area
+torrent.on('complete', function() {
+    console.log('complete!');
+    torrent.files.forEach(function(file) {
+        var newPath = '/public/files/' + file.path;
+        fs.rename(file.path, newPath);
+        // while still seeding need to make sure file.path points to the right place
+        file.path = newPath;
+		res.render('download', { name: file.path })
+    });
+});
+})
 module.exports = router;
