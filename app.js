246d1a84f39c2e48a369bd27cd125f04392c036c
Leonardo Mendes MIraglia <leo.mi.me@gmail.com>
2017-02-16 16:59:33 -0200

    list of itens

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ let	path = require('path'),
 	index = require('./routes/index'),
 	express = require('express'),
  	app = express(),
+	WebTorrent = require('webtorrent'),
+	torrent = new WebTorrent(),
  	server = require('http').Server(app),
- 	io = require('socket.io')(server);
+ 	io = require('socket.io')(server),
+	fs = require('fs'),
+	domain = require('domain').create();
 
 // view engine setup
+app.use('/downloads', express.static(__dirname  + 'public/files'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -20,11 +24,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.sockets.on('connection', function (client) {
+	function getFiles(dir, files_, fileType){
+
+    var regex = fileType ? new RegExp('\\' + fileType + '$') : '';
+
+    return fs.readdirSync(dir).reduce(function(allFiles, file){
+        var name = path.join(dir, file);
+        if (fs.statSync(name).isDirectory()){
+            getFiles(name, allFiles, fileType);
+        } else if (file.match(regex)){
+            allFiles.push(name);
+        }
+        return allFiles;
+    }, files_ || []);
+	}
+	console.log(getFiles(__dirname + '/public/files'));
+   	client.on('toServer', function (data) {
+	let magnetURI = data.magnet;
+	client.emit(data.id, { status: 1, files: list()});
+	torrent.add(magnetURI, { path: '/public/files' }, function (torrent) {
+	  torrent.on('done', function () {
+	 	client.emit(data.id, { status: 2, files : list()});
+	})
+   	});
+
+ })
 });
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,11 @@
 "use strict";
-var express = require('express');
-var router = express.Router();
-var WebTorrent = require('webtorrent')
-var client = new WebTorrent()
+ let express = require('express'),
+ router = express.Router();
 
-var fs = require('fs');
-var domain = require('domain').create();
-
-function list (){
-	fs.readdir('./public/files',function(error,files){
-	   return files;
-	});
-	domain.on("error",function(erros){
-	   console.log(erros);
-	});
-}
 
 router.get('/', function(req, res, next) {
-
-  res.render('index', { title: 'Express' });
-
+	var rand = (Math.random()+1).toString(36).substring(7);
+  	res.render('index', { rand: rand });
 });
-
-router.post('/torrent', (req, res, next) => {
-	let magnetURI = req.body.torrent;
-	console.log('here we go');
-	client.add(magnetURI, { path: '/public/files' }, function (torrent) {
-		console.log(torrent);
-	  torrent.on('done', function () {
-		console.log('torrent download finished')
-	  })
-	});
-});
-
 
 module.exports = router;
