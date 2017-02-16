b3ddfbe589cdf45672b8cde7b82fbf78568fc20a
Leonardo Mendes MIraglia <leo.mi.me@gmail.com>
2017-02-16 02:52:49 -0200

    post function error

--- routes/index.js
+++ routes/index.js
@@ -21,24 +21,19 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.post('/torrent', (err, req, res, next) => {
+router.post('/torrent', (req, res, next) => {
 	console.error(err.stack);
 	let magnet = req.body.torrent;
 	var torrent = client.addTorrent(magnet);
-	res.send(200);
 	// when the torrent completes, move it's files to another area
 	torrent.on('complete', function() {
-
     torrent.files.forEach(function(file) {
         var newPath = '/public/files/' + file.path;
         fs.rename(file.path, newPath);
-        // while still seeding need to make sure file.path points to the right place
-
         file.path = newPath;
-        res.json('download', { name: file.path })
-
+        res.json('download', { name: file.path });
     });
-
+	next();
 });
 
 })
