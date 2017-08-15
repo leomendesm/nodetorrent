2fff122f3ebe83606d06d8e2b8ecf59c5ef03d7b
LEONARDO <LEONARDO>
2017-08-15 20:57:01 -0300

    rota de delete

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
 	var rand = (Math.random()+1).toString(36).substring(7);
   	res.render('index', { rand: rand });
 });
-router.get('/file/delete/:arquivo', function(req, res, next) {
+router.get('/files/delete/:arquivo', function(req, res, next) {
 	fs.unlink(__dirname + '/public/files/'+req.params.arquivo, ()=>{
 		console.log('sucesso');
 	});
