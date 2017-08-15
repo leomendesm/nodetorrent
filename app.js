ed483173363a7b08ab977e1292248a7c11cdf480
LEONARDO <LEONARDO>
2017-08-15 20:40:59 -0300

    vm teste

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.get('/list',(req, res, next)=>{
 	res.render('list', { files : getFiles(__dirname + '/public/files') } )
 })
 io.sockets.on('connection', function (client) {
+	console.log('asd');
 	function getFiles(dir, files_, fileType){
 
     var regex = fileType ? new RegExp('\\' + fileType + '$') : '';
@@ -57,6 +58,7 @@ io.sockets.on('connection', function (client) {
     }, files_ || []);
 	}
    	client.on('toServer', function (data) {
+		console.log('asd')
 	let magnetURI = data.magnet;
 	client.emit(data.id, { status: 1});
 	torrent.add(magnetURI, { path: __dirname + '/public/files' }, function (torrent) {
